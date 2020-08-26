f29e86ac47af210ab52db48d5f657f2c97fc2e3f
Chad A. Carino <artist@chadacarino.com>
2020-08-26 11:43:38 -0600

    enhanced debug behavior

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
+/**
+ * Cloud Function Globals
+ * Executed ONCE upon deployment
+ */
+
 const request = require('request');
 const crypto = require('crypto');
 const util = require('util');
+const PROC_NAME = 'Cloud Function Monitoring to Azure';
+
+/**
+ * The environment variable ISDEBUG will turn on debugging behavior if defined
+ * @env * ISDEBUG enables debug logging if defined
+ */
+var isDebug = false;
+if ('undefined' !== typeof process.env.ISDEBUG) {
+	isDebug = true;
+}
+
+/* END Cloud Function Globals
+ **/
+
+/**
+ * Local Functions
+ * Callable ONLY within this module
+ */
+
 function PushToAzureLogs(content, {id, key, rfc1123date, LogType}, callback) {
-//     console.log(id);
+	
+	if (isDebug) {
+		console.log(PROC_NAME + ' - Azure Workspace ID: ' + id);
+	}
     try {
         //Checking if the data can be parsed as JSON
         if ( JSON.parse(JSON.stringify(content)) ) {
             var length = Buffer.byteLength(JSON.stringify(content),'utf8');
             var binaryKey = Buffer.from(key,'base64');
             var stringToSign = 'POST\n' + length + '\napplication/json\nx-ms-date:' + rfc1123date + '\n/api/logs';
-            //console.log(stringToSign)
+            
+            if (isDebug) {
+				console.log(PROC_NAME + ' - Azure Unhashed Auth: ' + stringToSign);
+			}
      
             var hash = crypto.createHmac('sha256',binaryKey)
             .update(stringToSign,'utf8')
@@ -31,22 +61,48 @@ function PushToAzureLogs(content, {id, key, rfc1123date, LogType}, callback) {
             request.post(uri, options, (err, Response) => {
                 //return if error inside try catch block 
                 if (err) {
-                    return callback(("Data could NOT be sent to Azure: " + err));
+                    return callback({
+						msg: "Error sending data to Azure",
+						res: Response,
+						err
+					});
                 }
-               return callback(("Data successfully sent to Azure " + util.inspect(content) + "with status code " + Response.statusCode));
-     
+                else if (Response && Response.statusCode === 200) {
+               		return callback({
+               			msg: "Data successfully sent to Azure " + util.inspect(content) + "with status code " + Response.statusCode,
+               			res: Response,
+               			err: null
+     			}
+     			else {
+     			
+     			}
             });
      
         }
     //Catch error if data cannot be parsed as JSON
     } catch (err) {
-        return callback(("Error sending data to Azure: " + err));
+        return callback({
+        	msg: "Error parsing JSON data",
+        	res: null,
+        	err
+        });
     }
             
 }
 
+/* END Local Functions
+ **/
+
 /**
- * Triggered from a message on a Cloud Pub/Sub topic.
+ * Exported Functions
+ * Callable as entry point for Cloud Function
+ * Executed on EACH invocation, not on deployment
+ */
+
+/**
+ * Testing function, logs the message data to console @ debug level.
+ * Triggered by a message on a Cloud Pub/Sub topic.
+ * Defaults to Hello World string if no parsable data in message.
  *
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
@@ -59,6 +115,7 @@ exports.helloPubSub = (event, context) => {
 };
 
 /**
+ * Sends message data to an Azure Log Analytics Workspace
  * Triggered from a message on a Cloud Pub/Sub topic.
  *
  * @param {!Object} event Event payload.
@@ -75,6 +132,15 @@ exports.sendMsgToAzure = (event, context) => {
         ? Buffer.from(event.data, 'base64').toString()
         : 'No Content';
     PushToAzureLogs(message, {'id': process.env.CUSTID, 'key': process.env.SHAREDKEY, 'rfc1123date': event.data.date, 'LogType': process.env.LOGTYPE}, (result) => {
-    	console.log(result);
+    	console.log(result.msg);
+    	if (isDebug && 'undefined' !== typeof result.err && null !== result.err) {
+    		if ('object' === typeof result.res && null !== result.res) {
+    			err.response = result.res;
+    		}
+    		console.error(util.inspect(err, {showHidden: false, depth: null});
+    	}
     });
 };
+
+/* END Exported Functions
+ **/
